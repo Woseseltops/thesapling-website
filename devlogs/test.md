f23939509eccd57e5ddb48bffdcc46e8fd9fe825
Wessel Stoop <stoopwessel@gmail.com>
2018-04-01 12:22:23 +0200

    Started with devlog mechanics

---
+++ devlogs/test.md
@@ -0,0 +1,16 @@
+Title
+=====
+
+This is the first alinea. This is the first alinea. This is the first alinea.
+
+This is a normal alinea. This is a normal alinea. This is a normal alinea. This is a normal alinea.
+
+---
+
+This is a pull quote
+
+---
+
+This is a normal alinea. This is a normal alinea. This is a normal alinea. This is a normal alinea.
+
+This is the last alinea.
--- generate_static_website.py
+++ generate_static_website.py
@@ -2,9 +2,13 @@ from os import listdir, mkdir
 from os.path import isdir
 from shutil import copytree, rmtree
 
+import markdown
+
 def generate_static_website():
 
 	PAGES_TO_GENERATE_FOLDER = 'pages_to_generate/'
+	DEVLOGS_FOLDER = 'devlogs/'
+
 	MAIN_TEMPLATE_LOCATION = 'main_template.html'
 	GOAL_LOCATION = 'latest_website/'
 	STATIC_FOLDER = 'static/'
@@ -12,11 +16,21 @@ def generate_static_website():
 	#Empty last
 	if isdir(GOAL_LOCATION):
 		rmtree(GOAL_LOCATION)
-		mkdir(GOAL_LOCATION)
 
+	mkdir(GOAL_LOCATION)
+	mkdir(GOAL_LOCATION+'devlogs/')
+
+	#Generate basic pages
 	for filename in listdir(PAGES_TO_GENERATE_FOLDER):
 		full_content = open(MAIN_TEMPLATE_LOCATION).read().replace('{{content}}',open(PAGES_TO_GENERATE_FOLDER+filename).read())
 		open(GOAL_LOCATION+filename,'w').write(full_content)
+
+	#Generate devlogs
+	for filename in listdir(DEVLOGS_FOLDER):
+		html = markdown.markdown(open(DEVLOGS_FOLDER+filename).read())
+		full_content = open(MAIN_TEMPLATE_LOCATION).read().replace('{{content}}',html)
+		filename = filename.replace('.md','.html')
+		open(GOAL_LOCATION+'devlogs/'+filename,'w').write(full_content)
 
 	copytree(STATIC_FOLDER,GOAL_LOCATION+STATIC_FOLDER)
 
