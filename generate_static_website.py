8cd5a66dfa3ed316a5bf37dadceb3c1ca63d29a3
Wessel Stoop <stoopwessel@gmail.com>
2018-05-21 19:36:21 +0200

    Wrote main logic for devlog list view

--- generate_static_website.py
+++ generate_static_website.py
@@ -5,10 +5,10 @@ from shutil import copytree, rmtree
 import datetime
 import markdown
 
-def parse_devlog(raw_markdown,tags):
+def parse_devlog(identifier,raw_markdown,tags):
 
 	#First, take out the info we need
-	devlog = DevLog()
+	devlog = DevLog(identifier)
 
 	markdown_lines = raw_markdown.split('\n')
 
@@ -49,6 +49,7 @@ def parse_devlog(raw_markdown,tags):
 
 		elif '<p>' in line:
 			if alinea_index == 0:
+				devlog.lead = html[n].replace('<p>','').replace('</p>','')
 				html[n] = html[n].replace('<p>','<p id="first_p">')
 				alinea_index += 1
 			elif alinea_index == 1:
@@ -63,25 +64,51 @@ def parse_devlog(raw_markdown,tags):
 			else:
 				pull_quote_mode = True
 
-	html.append('<p id="date">Published '+devlog.date.strftime("%A %d %B %Y")+'</p>')
+	html.append('<p id="date">Published '+devlog.get_pretty_date()+'</p>')
 
 	devlog.html = '\n'.join(html);
 	return devlog
 
 class DevLog():
 
-	def __init__(self):
+	def __init__(self,identifier):
+
+		self.identifier = identifier
 
 		self.title = ''
 		self.html = ''
 		self.timestamp = 0
 		self.tags = []
 		self.date = None
+		self.lead = ''
+
+	def get_pretty_date(self):
+		return self.date.strftime("%A %d %B %Y")		
+
+def fill_template(template_location,vars):
+
+	template = open(template_location).read()
+
+	for key, replacement in vars.items():
+		template = template.replace('{{'+key+'}}',replacement)
+	return template
 
 def create_page(template_location,title_area_file_location,title,content,page_type):
 
 	title_area = open(title_area_file_location).read()
-	return open(template_location).read().replace('{{title_area}}',title_area).replace('{{title}}',title).replace('{{content}}',content).replace('{{page_type}}',page_type)
+	return fill_template(template_location,{'title_area':title_area,'title':title,'content':content,'page_type':page_type})
+
+def devlog_to_list_item(devlog,devlog_list_item_template_location,even):
+
+	NR_OF_CHARS_IN_LEAD = 40;
+
+	if even:
+		evenorodd = 'even'
+	else:
+		evenorodd = 'odd'
+
+	return fill_template(devlog_list_item_template_location,{'identifier':devlog.identifier,'title':devlog.title,'date':devlog.get_pretty_date(),
+																'lead':devlog.lead[:NR_OF_CHARS_IN_LEAD],'tag':devlog.tags[0],'evenorodd':evenorodd})
 
 def generate_static_website():
 
@@ -91,6 +118,7 @@ def generate_static_website():
 	MAIN_TEMPLATE_LOCATION = 'main_template.html'
 	MAIN_TITLE_AREA_FILE_LOCATION = 'main_title_area.html'
 	DEVLOG_TITLE_AREA_FILE_LOCATION = 'devlog_title_area.html'
+	DEVLOG_LIST_ITEM_TEMPLATE_LOCATION = 'devlog_list_item_template.html'
 
 	GOAL_LOCATION = 'latest_website/'
 	STATIC_FOLDER = 'static/'
@@ -110,12 +138,25 @@ def generate_static_website():
 		open(GOAL_LOCATION+filename,'w').write(full_content)
 
 	#Generate devlogs
+	devlogs = []
+
 	for filename in listdir(DEVLOGS_FOLDER):
-		devlog = parse_devlog(open(DEVLOGS_FOLDER+filename).read(),DEVLOG_TAGS)
+		devlog = parse_devlog(filename.split('.')[0],open(DEVLOGS_FOLDER+filename).read(),DEVLOG_TAGS)
 		full_content = create_page(MAIN_TEMPLATE_LOCATION,DEVLOG_TITLE_AREA_FILE_LOCATION,devlog.title.upper(),devlog.html,'devlog')
 		filename = filename.replace('.md','.html')
 		open(GOAL_LOCATION+'devlogs/'+filename,'w').write(full_content)
+		devlogs.append(devlog)
 
+	#Generate the devlog listview
+	listview_content = '<h2>DEVLOGS</h2>'
+
+	for n,devlog in enumerate(devlogs):
+		listview_content += devlog_to_list_item(devlog,DEVLOG_LIST_ITEM_TEMPLATE_LOCATION,n%2==0)
+	
+	listview_content = create_page(MAIN_TEMPLATE_LOCATION,MAIN_TITLE_AREA_FILE_LOCATION,'The Sapling',listview_content,'main')
+	open(GOAL_LOCATION+'devlogs/index.html','w').write(listview_content)
+
+	#Move over the static files
 	copytree(STATIC_FOLDER,GOAL_LOCATION+STATIC_FOLDER)
 
 if __name__ == '__main__':
