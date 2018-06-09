71bacc02c682a0140e0d32117e958f0879ce3558
Wessel Stoop <stoopwessel@gmail.com>
2018-06-09 22:57:37 +0200

    Fixed title replacement bug

--- generate_static_website.py
+++ generate_static_website.py
@@ -86,8 +86,10 @@ class DevLog():
 
 def fill_template(template,vars):
 
-	for key, replacement in vars.items():
-		template = template.replace('{{'+key+'}}',replacement)
+	#Do it multiple times, as one iteration might create opportunities for the next one
+	for i in range(0,2):
+		for key, replacement in vars.items():
+			template = template.replace('{{'+key+'}}',replacement)
 
 	return template
 
